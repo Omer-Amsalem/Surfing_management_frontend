bcccc3bb7a5fbb5fc07e822faf38ac7224fd2221
Omer Amsalem <88431895+Omer-Amsalem@users.noreply.github.com>
2025-01-15 11:08:01 +0200

    Create a create post Page and adding (only for host)

--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -2,13 +2,14 @@ import {Routes, Route} from 'react-router-dom';
 import Login from '../pages/LogInPage';
 import Register from '../pages/RegisterPage';
 import HomePage from '../pages/HomePage';
+import CreatePostPage from '../pages/CreatePostPage';
 
 
 const Router = () => {
     return (
         <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/createPost" element={<HomePage />} />
+        <Route path="/createPost" element={<CreatePostPage />} />
         <Route path="/Home" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
